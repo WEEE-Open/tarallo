58d7725549d98bfb25442d3b27520ea61d3b961b
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-03-03 18:15:35 +0100

    Database semplificato ma non ancora abbastanza convincente

--- database.sql
+++ database.sql
@@ -5,30 +5,11 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-DROP DATABASE IF EXISTS `tarallo`;
 CREATE DATABASE `tarallo` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `tarallo`;
 
 SET NAMES utf8mb4;
 
-DROP TABLE IF EXISTS `DefaultItem`;
-CREATE TABLE `DefaultItem` (
-  `DefaultItemID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `Brand` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `Model` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`DefaultItemID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-DROP TABLE IF EXISTS `DefaultItemModification`;
-CREATE TABLE `DefaultItemModification` (
-  `ModificationID` bigint(20) unsigned NOT NULL,
-  `DefaultItemID` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`ModificationID`,`DefaultItemID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-DROP TABLE IF EXISTS `Feature`;
 CREATE TABLE `Feature` (
   `FeatureID` bigint(20) unsigned NOT NULL,
   `ItemID` bigint(20) unsigned NOT NULL,
@@ -37,19 +18,10 @@ CREATE TABLE `Feature` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-DROP TABLE IF EXISTS `FeatureDefault`;
-CREATE TABLE `FeatureDefault` (
-  `FeatureID` bigint(20) unsigned NOT NULL,
-  `DefaultItemID` bigint(20) unsigned NOT NULL,
-  `Value` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`FeatureID`,`DefaultItemID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-DROP TABLE IF EXISTS `Item`;
 CREATE TABLE `Item` (
   `ItemID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `ItemCode` bigint(20) unsigned NOT NULL,
+  `IsDefault` tinyint(1) NOT NULL,
   `Type` int(11) DEFAULT NULL,
   `Brand` text COLLATE utf8mb4_unicode_ci,
   `Model` text COLLATE utf8mb4_unicode_ci,
@@ -62,7 +34,6 @@ CREATE TABLE `Item` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-DROP TABLE IF EXISTS `ItemModification`;
 CREATE TABLE `ItemModification` (
   `ModificationID` bigint(20) unsigned NOT NULL,
   `ItemID` bigint(20) unsigned NOT NULL,
@@ -70,7 +41,6 @@ CREATE TABLE `ItemModification` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-DROP TABLE IF EXISTS `ItemPositionModification`;
 CREATE TABLE `ItemPositionModification` (
   `ModificationID` bigint(20) unsigned NOT NULL,
   `ParentFrom` bigint(20) unsigned NOT NULL,
@@ -79,7 +49,6 @@ CREATE TABLE `ItemPositionModification` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-DROP TABLE IF EXISTS `ItemSticker`;
 CREATE TABLE `ItemSticker` (
   `ItemID` bigint(20) unsigned NOT NULL,
   `Type` int(11) NOT NULL,
@@ -89,7 +58,6 @@ CREATE TABLE `ItemSticker` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-DROP TABLE IF EXISTS `Modification`;
 CREATE TABLE `Modification` (
   `ModificationID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `UserID` bigint(20) unsigned NOT NULL,
@@ -99,7 +67,6 @@ CREATE TABLE `Modification` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-DROP TABLE IF EXISTS `User`;
 CREATE TABLE `User` (
   `UserID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `Name` text COLLATE utf8mb4_unicode_ci NOT NULL,
