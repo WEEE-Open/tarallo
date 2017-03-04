dd88c87fc16bfbf0428e89a1fbe63ed14c0a8d7e
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-03-04 22:52:42 +0100

    Tabelle rinominate, nuove tabelle necessarie, vincoli d'integrità referenziale, passate varie ore a ragionare su come rappresentare la struttura dati dell'albero e le proprietà duplicate (ma con valori diversi), ancora nessuna soluzione ottimale è emersa.

--- database.sql
+++ database.sql
@@ -11,10 +11,9 @@ CREATE DATABASE `tarallo` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `tarallo`;
 
 CREATE TABLE `Feature` (
-  `FeatureID` bigint(20) unsigned NOT NULL,
-  `ItemID` bigint(20) unsigned NOT NULL,
-  `Value` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`FeatureID`,`ItemID`)
+  `FeatureID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `FeatureName` text NOT NULL,
+  PRIMARY KEY (`FeatureID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -30,22 +29,59 @@ CREATE TABLE `Item` (
   `Owner` text COLLATE utf8mb4_unicode_ci,
   `Borrowed` tinyint(1) DEFAULT NULL,
   `Notes` text COLLATE utf8mb4_unicode_ci,
-  PRIMARY KEY (`ItemID`)
+  PRIMARY KEY (`ItemID`),
+  KEY `Status` (`Status`),
+  KEY `Type` (`Type`),
+  CONSTRAINT `Item_ibfk_1` FOREIGN KEY (`Status`) REFERENCES `ItemStatus` (`StatusID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `Item_ibfk_2` FOREIGN KEY (`Type`) REFERENCES `ItemType` (`TypeID`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `ItemFeature` (
+  `FeatureID` bigint(20) unsigned NOT NULL,
+  `ItemID` bigint(20) unsigned NOT NULL,
+  `Value` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`FeatureID`,`ItemID`),
+  KEY `ItemID` (`ItemID`),
+  CONSTRAINT `ItemFeature_ibfk_1` FOREIGN KEY (`ItemID`) REFERENCES `Item` (`ItemID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `ItemFeature_ibfk_3` FOREIGN KEY (`FeatureID`) REFERENCES `Feature` (`FeatureID`) ON DELETE NO ACTION ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `ItemLocationModification` (
+  `ModificationID` bigint(20) unsigned NOT NULL,
+  `ParentFrom` bigint(20) unsigned NOT NULL,
+  `ParentTo` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`ModificationID`,`ParentFrom`,`ParentTo`),
+  KEY `ParentFrom` (`ParentFrom`),
+  KEY `ParentTo` (`ParentTo`),
+  CONSTRAINT `ItemLocationModification_ibfk_2` FOREIGN KEY (`ModificationID`) REFERENCES `Modification` (`ModificationID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `ItemLocationModification_ibfk_3` FOREIGN KEY (`ParentFrom`) REFERENCES `Item` (`ItemID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `ItemLocationModification_ibfk_4` FOREIGN KEY (`ParentTo`) REFERENCES `Item` (`ItemID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 CREATE TABLE `ItemModification` (
   `ModificationID` bigint(20) unsigned NOT NULL,
   `ItemID` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`ModificationID`,`ItemID`)
+  PRIMARY KEY (`ModificationID`,`ItemID`),
+  KEY `ItemID` (`ItemID`),
+  CONSTRAINT `ItemModification_ibfk_1` FOREIGN KEY (`ModificationID`) REFERENCES `Modification` (`ModificationID`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `ItemModification_ibfk_3` FOREIGN KEY (`ItemID`) REFERENCES `Item` (`ItemID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-CREATE TABLE `ItemPositionModification` (
-  `ModificationID` bigint(20) unsigned NOT NULL,
-  `ParentFrom` bigint(20) unsigned NOT NULL,
-  `ParentTo` bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (`ModificationID`,`ParentFrom`,`ParentTo`)
+CREATE TABLE `ItemStatus` (
+  `StatusID` int(11) NOT NULL,
+  `StatusText` text NOT NULL,
+  PRIMARY KEY (`StatusID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+CREATE TABLE `ItemType` (
+  `TypeID` int(11) NOT NULL,
+  `TypeText` text NOT NULL,
+  PRIMARY KEY (`TypeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -54,7 +90,9 @@ CREATE TABLE `Modification` (
   `UserID` bigint(20) unsigned NOT NULL,
   `Date` datetime NOT NULL,
   `Notes` text COLLATE utf8mb4_unicode_ci,
-  PRIMARY KEY (`ModificationID`)
+  PRIMARY KEY (`ModificationID`),
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `Modification_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
