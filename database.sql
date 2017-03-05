09918d77e575faaa28bd83eaf40f33d050b02caa
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-03-05 20:17:48 +0100

    Charset di default opportuno

--- database.sql
+++ database.sql
@@ -1,13 +1,9 @@
--- Adminer 4.2.5 MySQL dump
-
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
-SET NAMES utf8mb4;
-
-CREATE DATABASE `tarallo` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE `tarallo` DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `tarallo`;
 
 CREATE TABLE `Feature` (
