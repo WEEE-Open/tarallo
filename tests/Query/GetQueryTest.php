19d859023b8a8ace3f0a0a0a7899d919228d55aa
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-03-19 23:25:11 +0100

    Altri test
    Raggiunta copertura del 100% del codice su alcune classi.
    Che però sono di 3 righe l'una, quindi non conta molto.

--- tests/Query/GetQueryTest.php
+++ tests/Query/GetQueryTest.php
@@ -5,6 +5,19 @@ namespace WEEEOpen\Tarallo\Test\Query;
 use PHPUnit\Framework\TestCase;
 use WEEEOpen\Tarallo\Query\GetQuery;
 
+/*
+ * Why are there tests covering classes other than GetQuery?
+ * Well, that functionality could have been implemented in GetQuery, even if
+ * it's elsewhere, it should be marked as tested and covered.
+ * The important thing is that those query strings should give the expected
+ * results (exceptions, etc...), writing a million tests for every method
+ * in every abstract class that shouldn't even be accessed durectly by the
+ * application seems a bit pointless. Maybe these tests aren't of impeccable
+ * quality, but they're better than no tests at all and I can change how
+ * GetQuery implements its functionality and see if I've broken anything
+ * without rewriting half of the tests, which is the whole point of this
+ * effort...
+ */
 class GetQueryTest extends TestCase {
 	/**
 	 * @covers \WEEEOpen\Tarallo\Query\GetQuery
@@ -15,6 +28,28 @@ class GetQueryTest extends TestCase {
 	public function testBuildTwice() {
 		$this->expectException(\LogicException::class);
 		(new GetQuery())->fromString('/Location/test')->fromString('/Location/test');
+	}
+
+	/**
+	 * @covers \WEEEOpen\Tarallo\Query\GetQuery
+	 * @uses   \WEEEOpen\Tarallo\Query\QueryFieldLocation
+	 * @uses   \WEEEOpen\Tarallo\Query\QueryFieldMultifield
+	 * @uses   \WEEEOpen\Tarallo\Query\AbstractQuery
+	 * @uses   \WEEEOpen\Tarallo\Query\QueryFieldDepth
+	 * @covers \WEEEOpen\Tarallo\Query\QueryFieldSinglefield
+	 */
+	public function testDuplicateField() {
+		$this->expectException(\InvalidArgumentException::class);
+		(new GetQuery())->fromString('/Depth/2/Depth/6');
+	}
+
+	/**
+	 * @covers \WEEEOpen\Tarallo\Query\GetQuery
+	 * @uses   \WEEEOpen\Tarallo\Query\AbstractQuery
+	 */
+	public function testUnrecognizedField() {
+		$this->expectException(\InvalidArgumentException::class);
+		(new GetQuery())->fromString('/NotAField/test');
 	}
 
 	/**
