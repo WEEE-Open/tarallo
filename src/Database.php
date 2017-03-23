b32fbc9ccb0956c67afba85d8f19ff88390803ec
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-03-23 17:10:04 +0100

    Database, parte 1 di 23895

--- src/Database.php
+++ src/Database.php
@@ -3,28 +3,29 @@
 namespace WEEEOpen\Tarallo;
 
 class Database {
-	static $pdo = null;
+	private static $pdo = null;
+
+	private function getPDO() {
+		if(self::$pdo === null) {
+			$this->connect(DB_USERNAME, DB_PASSWORD, DB_DSN);
+		}
+		return self::$pdo;
+	}
 
 	private function connect($user, $pass, $dsn) {
-		if(self::$pdo != null) {
-			return;
-		}
-
 		try {
-			self::$pdo = new \PDO($dsn, $user, $pass);
+			self::$pdo = new \PDO($dsn, $user, $pass, [
+				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+				\PDO::ATTR_CASE => \PDO::CASE_NATURAL,
+				\PDO::ATTR_AUTOCOMMIT => false,
+				\PDO::ATTR_EMULATE_PREPARES => false,
+			]);
 		} catch (\PDOException $e) {
 			throw new \Exception('Cannot connect to database: ' . $e->getMessage());
 		}
 	}
 
 	private function disconnect() {
-
 		self::$pdo = null;
 	}
-
-	// Don't implement this:
-	//public function getPDO() {
-	//
-	//}
-	// Make a function for each query instead!
 }
