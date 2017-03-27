3c673714518f1b95e6dcedafffe8c4459d12cbf4
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-03-27 20:40:12 +0200

    Try-catch gigante o quasi

--- index.php
+++ index.php
@@ -34,14 +34,23 @@ if($_SERVER['REQUEST_METHOD'] === 'GET') {
 
 assert(isset($query));
 
-$db = new Database();
-$user = Session::restore($db);
+try {
+	$db   = new Database();
+	$user = Session::restore($db);
+} catch(\Exception $e) {
+	Response::sendError('Error: ' . $e->getMessage());
+	assert(isset($user)); // pointless, sendError exit()s, this just stops the IDE from throwing warnings at me
+}
 
 if($query instanceof Query\PostQuery) {
 	// not really sold on this design, too much complexity hidden into a single function. But these objects ARE queries,
 	// it doesn't make much sense to extract everything and "parse" it again to convert it to a SQL query somewhere else...
-	$data = $query->run($user, $db);
-	Response::sendSuccess((array) $data);
+	try {
+		$data = $query->run($user, $db);
+		Response::sendSuccess((array) $data);
+	} catch(\Exception $e) {
+		Response::sendError('Error: ' . $e->getMessage());
+	}
 } else {
 	Response::sendSuccess([(string) $query]);
 }
