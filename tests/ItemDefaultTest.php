dd6928c69c2eeb1ffa18a8da9a77a0a9145f1051
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-04-15 12:54:03 +0200

    Test di ItemDefault
    Copertura del 100% della classe raggiunta. Anche perché ha 3 metodi e basta...

---
+++ tests/ItemDefaultTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use WEEEOpen\Tarallo\ItemDefault;
+
+class ItemDefaultTest extends TestCase {
+
+	/**
+	 * @covers         \WEEEOpen\Tarallo\ItemDefault
+	 * @uses           \WEEEOpen\Tarallo\ItemIncomplete
+	 * @uses           \WEEEOpen\Tarallo\Item
+	 */
+	public function testItemValidCodeString() {
+		$pc77 = new ItemDefault('PC-77');
+		$this->assertEquals('PC-77', (string) $pc77);
+		$this->assertEquals('PC-77', $pc77->getCode());
+		$this->assertEmpty($pc77->getFeatures());
+		$this->assertEmpty($pc77->getContent());
+		$this->assertEmpty($pc77->getFeaturesDefault());
+		$this->assertNull($pc77->getDefaultCode());
+		$decoded = json_decode(json_encode($pc77), true);
+		$this->assertArrayHasKey('is_default', $decoded);
+		$this->assertTrue($decoded['is_default']);
+	}
+
+	/**
+	 * @covers         \WEEEOpen\Tarallo\ItemDefault
+	 * @uses           \WEEEOpen\Tarallo\ItemIncomplete
+	 * @uses           \WEEEOpen\Tarallo\Item
+	 */
+	public function testItemFeature() {
+		$item = new ItemDefault('TEST');
+		$item->addFeature('capacity-byte', 9001);
+		$this->assertArrayHasKey('capacity-byte', $item->getFeatures());
+		$this->assertEquals(9001, $item->getFeatures()['capacity-byte']);
+		$this->assertEmpty($item->getFeaturesDefault());
+	}
+
+	/**
+	 * @covers         \WEEEOpen\Tarallo\ItemDefault
+	 * @uses           \WEEEOpen\Tarallo\ItemIncomplete
+	 * @uses           \WEEEOpen\Tarallo\Item
+	 * @depends        testItemFeature
+	 */
+	public function testItemMultipleFeaturesArray() {
+		$item = new ItemDefault('TEST');
+		$item->addMultipleFeatures(['capacity-byte' => 9001, 'color' => 'white', 'foo' => 'bar'])->addFeature('test', 'test');
+		$this->assertEmpty($item->getFeaturesDefault());
+	}
+
+	/**
+	 * @covers         \WEEEOpen\Tarallo\ItemDefault
+	 * @uses           \WEEEOpen\Tarallo\ItemIncomplete
+	 * @uses           \WEEEOpen\Tarallo\Item
+	 */
+	public function testInvalidAddFeatureDefault() {
+		$this->expectException(LogicException::class);
+		(new ItemDefault('TEST'))->addFeatureDefault('capacity-byte', 9002);
+	}
+}
