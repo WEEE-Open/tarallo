8f8088f3938ad9dd052dd64c695a20bf56aabc01
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-02-22 13:04:16 +0100

    Scisma delle procedure

---
+++ database-procedures.sql
@@ -0,0 +1,144 @@
+
+-- Now we're getting real.
+
+DROP FUNCTION IF EXISTS GenerateCode;
+DELIMITER $$
+CREATE FUNCTION GenerateCode(currentPrefix varchar(20))
+	RETURNS varchar(190)
+MODIFIES SQL DATA
+	-- This means that in two identical databases, with the same values everywhere, the function produces the same
+	-- results, which is useful to know for replication. Setting to deterministic also enables some optimizations,
+	-- apparently. However many people on TEH INTERNETS say that anything other than a pure function is nonderministic,
+	-- so who knows! If the database crashes and burns, we'll know it wasn't actually deterministic.
+DETERMINISTIC
+	BEGIN
+		DECLARE thePrefix varchar(20) CHARACTER SET 'utf8mb4'
+		COLLATE 'utf8mb4_unicode_ci';
+		DECLARE theInteger bigint UNSIGNED;
+		DECLARE duplicateExists boolean;
+		DECLARE newCode varchar(190) CHARACTER SET 'utf8mb4'
+		COLLATE 'utf8mb4_unicode_ci';
+
+		SELECT Prefix, `Integer`
+		INTO thePrefix, theInteger
+		FROM Prefixes
+		WHERE Prefix = currentPrefix;
+
+		IF (thePrefix IS NOT NULL)
+		THEN
+			REPEAT
+				SET theInteger = theInteger + 1;
+				SET NewCode = CONCAT(thePrefix, CAST(theInteger AS char(20)));
+
+				SELECT IF(COUNT(*) > 0, TRUE, FALSE)
+				INTO duplicateExists
+				FROM Item
+				WHERE Code = NewCode;
+			UNTIL duplicateExists = FALSE
+			END REPEAT;
+
+			UPDATE Prefixes
+			SET `Integer` = theInteger
+			WHERE Prefix = thePrefix;
+
+			RETURN newCode;
+		ELSE
+			RETURN NULL;
+		END IF;
+
+	END$$
+DELIMITER ;
+
+-- TODO: extend and use to log in?
+DROP PROCEDURE IF EXISTS SetUser;
+DELIMITER $$
+CREATE PROCEDURE SetUser(IN username varchar(100) CHARACTER SET 'utf8mb4')
+	BEGIN
+		SET @taralloAuditUsername = username;
+	END$$
+
+DROP FUNCTION IF EXISTS GetParent;
+CREATE FUNCTION GetParent(child varchar(100))
+	RETURNS varchar(100)
+READS SQL DATA
+DETERMINISTIC
+	BEGIN
+		DECLARE found varchar(100);
+		SELECT Ancestor
+		INTO found
+		FROM Tree
+		WHERE Descendant = child
+			AND Depth = 1;
+		RETURN found;
+	END$$
+DELIMITER ;
+
+-- TODO: update expiration date, too
+DROP TRIGGER IF EXISTS SearchResultsDelete;
+DELIMITER $$
+CREATE TRIGGER SearchResultsDelete
+	AFTER DELETE
+	ON SearchResult
+	FOR EACH ROW -- MySQL doesn't have statement-level triggers. Excellent piece of software, I must say.
+	BEGIN
+		UPDATE Search
+		SET ResultsCount = ResultsCount - 1
+		WHERE Code = OLD.Search;
+	END $$
+DELIMITER ;
+
+-- These triggers can't be added, BTW, because UPDATE and DELETE are the same action for MySQL apparently and it can't have two triggers for the same action
+DROP TRIGGER IF EXISTS SearchResultsUpdate;
+DELIMITER $$
+CREATE TRIGGER SearchResultsUpdate
+	AFTER UPDATE -- Also can't specify UPDATE of what.
+	ON SearchResult
+	FOR EACH ROW
+	BEGIN
+		-- "UPDATE OF Search"
+		IF (OLD.Search != NEW.Search)
+		THEN
+			UPDATE Search
+			SET ResultsCount = ResultsCount - 1
+			WHERE Code = OLD.Search;
+			UPDATE Search
+			SET ResultsCount = ResultsCount - 1
+			WHERE Code = OLD.Search;
+		END IF;
+	END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS SearchResultsInsert;
+DELIMITER $$
+CREATE TRIGGER SearchResultsInsert
+	AFTER INSERT
+	ON SearchResult
+	FOR EACH ROW -- This may kill performance...
+	BEGIN
+		UPDATE Search
+		SET ResultsCount = ResultsCount + 1
+		WHERE Code = NEW.Search;
+	END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS SetRealSearchResultTimestampBecauseMySQLCant;
+DELIMITER $$
+CREATE TRIGGER SetRealSearchResultTimestampBecauseMySQLCant
+	BEFORE INSERT
+	ON Search
+	FOR EACH ROW
+	BEGIN
+		SET NEW.Expires = TIMESTAMPADD(HOUR, 6, CURRENT_TIMESTAMP);
+	END $$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS SetRealSearchResultTimestampBecauseMySQLCantAgain;
+DELIMITER $$
+CREATE TRIGGER SetRealSearchResultTimestampBecauseMySQLCantAgain
+	BEFORE UPDATE
+	ON Search
+	FOR EACH ROW
+	BEGIN
+		SET NEW.Expires = TIMESTAMPADD(HOUR, 6, CURRENT_TIMESTAMP);
+	END $$
+DELIMITER ;
