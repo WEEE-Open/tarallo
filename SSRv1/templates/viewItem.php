411162e20a1dbc204092f9844e1de3c8a0a6ed85
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-02-28 12:36:51 +0100

    Working yes/no/maybe

--- SSRv1/templates/viewItem.php
+++ SSRv1/templates/viewItem.php
@@ -1,18 +1,43 @@
 <?php
 /** @var \WEEEOpen\Tarallo\Server\User $user */
 /** @var \WEEEOpen\Tarallo\Server\Item $item */
-if(!isset($recursion) || $recursion === false)
-	$this->layout('internalPage', ['title' => 'Visualizza', 'user' => $user]) ?>
+if(!isset($recursion) || $recursion === false) {
+	$recursion = false;
+	$this->layout('internalPage', ['title' => 'Visualizza', 'user' => $user]);
+} else {
+	$recursion = true;
+}
 
-<article class="item">
-	<section class="breadbox">
+$features = $item->getCombinedFeatures();
+
+// Too much logic in a template, blah bla blah... this templates renders an Item in every respect,
+// what would I gain by moving this logic outside and passing $works as a parameter? More code, scattering
+// templates-related stuff around (Adapter and other classes don't care if items are working or broken!),
+// duplicating information, increasing probability of introducing bugs?
+$working = '';
+if(isset($features['working'])) {
+	$value = $features['working']->value;
+	switch($value) {
+		case 'yes':
+		case 'no':
+		case 'maybe':
+			$working = "working $value";
+			break;
+	}
+	unset($value);
+}
+
+?>
+
+<article class="item <?= $recursion ? '' : 'head' ?> <?= $working ?>">
+	<nav class="breadbox">
 		<ul class="breadcrumbs">
 			<?php foreach($item->getPath() as $piece): ?>
 				<li><a href="/item/<?=$this->u($piece)?>"><?=$this->e($piece)?></a></li>
 			<?php endforeach; ?>
 		</ul>
 		<!--<div class="breadsetter"><label>Set parent: <input></label></div>-->
-	</section>
+	</nav>
 
 	<header>
 		<h2><?=$this->e($item->getCode())?></h2>
@@ -20,12 +45,11 @@ if(!isset($recursion) || $recursion === false)
 
 	<section class="features">
 		<?php
-		$features = $item->getCombinedFeatures();
 		if(count($features) > 0): ?>
 			<section>
 				<h3>All features (no grouping yet)</h3>
 				<ul>
-					<?php foreach($item->getCombinedFeatures() as $feature): ?>
+					<?php foreach($features as $feature): ?>
 						<li>
 							<div class="name"><?=$feature->name?></div>
 							<div class="value"><?=$this->printFeatureValue($feature)?></div>
