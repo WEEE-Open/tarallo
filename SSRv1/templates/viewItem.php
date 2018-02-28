51eb690e75d166cd2ecd3115f1cb857894364415
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-02-28 11:07:34 +0100

    "Grafica" feature

--- SSRv1/templates/viewItem.php
+++ SSRv1/templates/viewItem.php
@@ -1,20 +1,21 @@
 <?php
 /** @var \WEEEOpen\Tarallo\Server\User $user */
 /** @var \WEEEOpen\Tarallo\Server\Item $item */
-$this->layout('internalPage', ['title' => 'Visualizza', 'user' => $user]) ?>
+if(!isset($recursion) || $recursion === false)
+	$this->layout('internalPage', ['title' => 'Visualizza', 'user' => $user]) ?>
 
 <article class="item">
 	<section class="breadbox">
 		<ul class="breadcrumbs">
 			<?php foreach($item->getPath() as $piece): ?>
-				<li><a href="/item/<?= $this->u($piece) ?>"><?= $this->e($piece) ?></a></li>
+				<li><a href="/item/<?=$this->u($piece)?>"><?=$this->e($piece)?></a></li>
 			<?php endforeach; ?>
 		</ul>
 		<!--<div class="breadsetter"><label>Set parent: <input></label></div>-->
 	</section>
 
 	<header>
-		<h1><?= $this->e($item->getCode()) ?></h1>
+		<h2><?=$this->e($item->getCode())?></h2>
 	</header>
 
 	<section class="features">
@@ -22,16 +23,25 @@ $this->layout('internalPage', ['title' => 'Visualizza', 'user' => $user]) ?>
 		$features = $item->getCombinedFeatures();
 		if(count($features) > 0): ?>
 			<section>
-				<h2>All features (no grouping, yet)</h2>
+				<h3><?=_('All features (no grouping, yet)')?></h3>
 				<ul>
-			<?php foreach($item->getCombinedFeatures() as $feature): ?>
-				<li><div><?= $feature->name ?></div><div><?= $this->printFeatureValue($feature) ?></div></li>
-			<?php endforeach; ?>
+					<?php foreach($item->getCombinedFeatures() as $feature): ?>
+						<li>
+							<div class="name"><?=$feature->name?></div>
+							<div class="value"><?=$this->printFeatureValue($feature)?></div>
+						</li>
+					<?php endforeach; ?>
 				</ul>
 			</section>
 		<?php endif; ?>
 	</section>
 
 	<section class="subitems">
+		<?php
+			$subitems = $item->getContents();
+			foreach($subitems as $subitem) {
+				$this->insert('viewItem', ['item' => $subitem, 'user' => $user, 'recursion' => true]);
+			}
+		?>
 	</section>
 </article>
