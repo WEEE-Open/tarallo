24741976d94d7f4de09b1ae5c89446f2cfc8a740
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-03-20 22:14:05 +0100

    Mancava un pezzo di paginazione

---
+++ SSRv1/templates/pagination.php
@@ -0,0 +1,23 @@
+<?php
+/** @var int $searchId */
+/** @var int $page Current page */
+/** @var int $pages Total number of pages */
+?>
+<div class="pagination">
+	<?php if($page === 1):
+		?><a class="disabled">← Previous</a><?php
+	else:
+		?><a href="<?php printf('/search/%d/page/%d', $searchId, $page - 1) ?>">← Previous</a><?php
+	endif;
+	for($i = 1; $i <= $pages; $i++):
+		if($i === $page):
+			?><a class="disabled"><?=$i?></a><?php
+		else:
+			?><a href="<?php printf('/search/%d/page/%d', $searchId, $i) ?>"><?=$i?></a><?php
+		endif;
+	endfor;
+	if($page === $pages):
+		?><a class="disabled">Next →</a><?php
+	else:
+		?><a href="<?php printf('/search/%d/page/%d', $searchId, $page + 1) ?>">Next →</a><?php
+	endif ?></div>
