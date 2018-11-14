4c66bc21c88aae0fbf89fa64d387a7694264ba42
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2018-11-14 10:57:50 +0100

    Back to tabs

--- src/Database/StatsDAO.php
+++ src/Database/StatsDAO.php
@@ -5,10 +5,10 @@ namespace WEEEOpen\Tarallo\Server\Database;
 use WEEEOpen\Tarallo\Server\Feature;
 
 final class StatsDAO extends DAO {
-    public function getLocationsByItems() {
-        $array = [];
+	public function getLocationsByItems() {
+		$array = [];
 
-        $result = $this->getPDO()->query('SELECT `Code` AS Location, COUNT(*) - 1 AS Descendants
+		$result = $this->getPDO()->query('SELECT `Code` AS Location, COUNT(*) - 1 AS Descendants
 FROM ItemFeature, Tree
 WHERE ItemFeature.Code = Tree.Ancestor
 AND ItemFeature.Feature = \'type\'
@@ -16,65 +16,65 @@ AND ItemFeature.ValueEnum = \'location\'
 GROUP BY Tree.Ancestor
 ORDER BY COUNT(*) DESC, Location ASC;', \PDO::FETCH_ASSOC);
 
-        if($result === false) {
-            throw new DatabaseException('Available locations query failed for no reason');
-        }
+		if($result === false) {
+			throw new DatabaseException('Available locations query failed for no reason');
+		}
 
-        try {
-            foreach($result as $row) {
-                $array[$row['Location']] = $row['Descendants'];
-            }
-        } finally {
-            $result->closeCursor();
-        }
+		try {
+			foreach($result as $row) {
+				$array[$row['Location']] = $row['Descendants'];
+			}
+		} finally {
+			$result->closeCursor();
+		}
 
-        return $array;
-    }
+		return $array;
+	}
 
-    public function getDuplicateSerialsCount() {
-        $array = [];
+	public function getDuplicateSerialsCount() {
+		$array = [];
 
-        $result = $this->getPDO()->query('SELECT ValueText AS SN, COUNT(*) AS Count
+		$result = $this->getPDO()->query('SELECT ValueText AS SN, COUNT(*) AS Count
 FROM ItemFeature
 WHERE Feature = \'sn\'
 GROUP BY ValueText
 HAVING Count > 1
 ORDER BY Count DESC, SN ASC', \PDO::FETCH_ASSOC);
 
-        if($result === false) {
-            throw new DatabaseException('Duplicate serial numbers query failed for no reason');
-        }
+		if($result === false) {
+			throw new DatabaseException('Duplicate serial numbers query failed for no reason');
+		}
 
-        try {
-            foreach($result as $row) {
-                $array[$row['SN']] = $row['Count'];
-            }
-        } finally {
-            $result->closeCursor();
-        }
+		try {
+			foreach($result as $row) {
+				$array[$row['SN']] = $row['Count'];
+			}
+		} finally {
+			$result->closeCursor();
+		}
 
-        return $array;
-    }
+		return $array;
+	}
 
-    /**
-     * Get most/least recently changed cases in a particular location, excluding in-use ones. This takes into account
-     * all audit entries for all contained items.
-     *
-     * Any attempt to make the function more generic failed miserably or was escessively complex, but consider
-     * that this is a very specific kind of stat to begin with...
-     * @todo parametrize the "in-use" exclusion, maybe? So the "most recently modified" makes more sense
-     * @todo try to parametrize the "type=case" filter
-     *
-     * @param string $location Where to look
-     * @param bool $recent True for more recently modified items first, false for least recently modified
-     * @param int $limit rows to return
-     *
-     * @return int[] code => timestamp
-     */
-    public function getModifiedItems(string $location, bool $recent = true, int $limit = 100): array {
-        $array = [];
+	/**
+	 * Get most/least recently changed cases in a particular location, excluding in-use ones. This takes into account
+	 * all audit entries for all contained items.
+	 *
+	 * Any attempt to make the function more generic failed miserably or was escessively complex, but consider
+	 * that this is a very specific kind of stat to begin with...
+	 * @todo parametrize the "in-use" exclusion, maybe? So the "most recently modified" makes more sense
+	 * @todo try to parametrize the "type=case" filter
+	 *
+	 * @param string $location Where to look
+	 * @param bool $recent True for more recently modified items first, false for least recently modified
+	 * @param int $limit rows to return
+	 *
+	 * @return int[] code => timestamp
+	 */
+	public function getModifiedItems(string $location, bool $recent = true, int $limit = 100): array {
+		$array = [];
 
-        $query = "SELECT `Ancestor` AS `Item`, `Time`, UNIX_TIMESTAMP(MAX(`Time`)) AS `Last`
+		$query = "SELECT `Ancestor` AS `Item`, `Time`, UNIX_TIMESTAMP(MAX(`Time`)) AS `Last`
 FROM Audit
 JOIN Tree ON Tree.Descendant=Audit.Code
 	WHERE `Ancestor` IN (
@@ -95,34 +95,35 @@ AND `Ancestor` NOT IN (
 GROUP BY `Ancestor`
 ORDER BY `Last` " . ($recent ? 'DESC' : 'ASC') . '
 LIMIT :lim';
-        $statement = $this->getPDO()->prepare($query);
+		$statement = $this->getPDO()->prepare($query);
 
-        $statement->bindValue(':loc', $location);
-        $statement->bindValue(':lim', $limit, \PDO::PARAM_INT);
+		$statement->bindValue(':loc', $location);
+		$statement->bindValue(':lim', $limit, \PDO::PARAM_INT);
 
-        try {
-            $success = $statement->execute();
-            assert($success);
+		try {
+			$success = $statement->execute();
+			assert($success);
 
-            while($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
-                $array[$row['Item']] = $row['Last'];
-            }
-        } finally {
-            $statement->closeCursor();
-        }
+			while($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+				$array[$row['Item']] = $row['Last'];
+			}
+		} finally {
+			$statement->closeCursor();
+		}
 
-        return $array;
-    }
+		return $array;
+	}
 
-    public function getCountByFeature(string $feature, Feature $filter){
-        if(Feature::getType($feature) != Feature::STRING)
-            throw new \LogicException('Feature must be a ValueText type');
+	public function getCountByFeature(string $feature, Feature $filter) {
+		if(Feature::getType($feature) != Feature::STRING) {
+			throw new \LogicException('Feature must be a ValueText type');
+		}
 
-        $array = [];
+		$array = [];
 
-        $type = Feature::getColumn($filter->type);
+		$type = Feature::getColumn($filter->type);
 
-        $query = "SELECT ValueText, COUNT(*) AS Quanti
+		$query = "SELECT ValueText, COUNT(*) AS Quanti
 FROM ItemFeature
 WHERE Feature = '" . $feature . "'
 AND Code IN (
@@ -133,21 +134,22 @@ AND Code IN (
 GROUP BY ValueText
 ORDER BY Quanti DESC";
 
-        $statement = $this->getPDO()->prepare($query);
+		$statement = $this->getPDO()->prepare($query);
 
-        $pdoType = $filter->value === Feature::INTEGER ? \PDO::PARAM_INT : \PDO::PARAM_STR;
+		$pdoType = $filter->value === Feature::INTEGER ? \PDO::PARAM_INT : \PDO::PARAM_STR;
 
-        $statement->bindValue(':val', $filter->value, $pdoType);
-        $statement->bindValue(':nam', $filter->name, \PDO::PARAM_STR);
-        try {
-            $success = $statement->execute();
-            assert($success);
-            while ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
-                $array[$row['ValueText']] = $row['Quanti'];
-            }
-        } finally {
-            $statement->closeCursor();
-        }
-        return $array;
-    }
+		$statement->bindValue(':val', $filter->value, $pdoType);
+		$statement->bindValue(':nam', $filter->name, \PDO::PARAM_STR);
+		try {
+			$success = $statement->execute();
+			assert($success);
+			while($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+				$array[$row['ValueText']] = $row['Quanti'];
+			}
+		} finally {
+			$statement->closeCursor();
+		}
+
+		return $array;
+	}
 }
