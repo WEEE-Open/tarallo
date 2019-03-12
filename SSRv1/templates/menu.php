2b876801ebdb237d888c1ee314f7245b407c61e8
Quadrollopo <davide97@gmail.com>
2019-03-12 16:55:20 +0100

    Added move2 (Temporary name asd), and moved function

--- SSRv1/templates/menu.php
+++ SSRv1/templates/menu.php
@@ -14,6 +14,7 @@
 		><li><a role="button" tabindex="0" class="quick" data-toggle="move">Move</a></li
 		><li><a href="/search">Search</a></li
 		><li><a href="/stats">Stats</a></li
+        ><li><a href="/move2">Move.2</a></li
 		><li><a href="/options">Options</a></li>
 	</ul>
 </nav>
