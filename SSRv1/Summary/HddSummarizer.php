720dfde97996c2fc3d2bf64638f3340260397206
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-06-01 21:55:14 +0200

    Empty HDD summarizer class

---
+++ SSRv1/Summary/HddSummarizer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WEEEOpen\Tarallo\SSRv1\Summary;
+
+
+use WEEEOpen\Tarallo\Server\ItemFeatures;
+
+class HddSummarizer implements Summarizer {
+
+	public static function summarize(ItemFeatures $item): string {
+		// TODO: Implement summarize() method.
+		return '';
+	}
+}
