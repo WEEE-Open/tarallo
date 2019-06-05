2b62fd78bdbb7452905d8624c3f7c1cd97e020d3
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-06-05 12:48:47 +0200

    Better history

--- SSRv1/templates/history.php
+++ SSRv1/templates/history.php
@@ -6,7 +6,7 @@
 $this->layout('main', ['title' => $item->getCode() . ' history', 'user' => $user, 'itembuttons' => true]);
 ?>
 
-<?php if(!$recursion): $this->insert('breadcrumbs', ['item' => $item]); endif; ?>
+<?php $this->insert('breadcrumbs', ['item' => $item]); ?>
 <article class="item root<?= $deleted ? ' deleted' : '' ?>"
 		data-code="<?=$this->e($item->getCode())?>">
 	<header>
@@ -24,23 +24,30 @@ $this->layout('main', ['title' => $item->getCode() . ' history', 'user' => $user
 		<?php if(empty($history)): ?>
 			<p>Nothing to show</p>
 		<?php else: ?>
-			<p>This is still in beta. Actions in reverse order (most recent first), M means Move (to the "Other" column), C create, U update.</p>
 			<table>
 				<thead>
 				<tr>
-					<td>Change</td>
-					<td>Other</td>
-					<td>Time</td>
-					<td>Operator</td>
+                    <td>Time</td>
+                    <td>Operator</td>
+                    <td>Action</td>
 				</tr>
 				</thead>
 				<tbody>
 				<?php date_default_timezone_set('Europe/Rome'); foreach($history as $row): ?>
 					<tr>
-						<td><?= $row['change'] ?></td>
-						<td><?= $row['other'] ?? '' ?></td>
-						<td><?=date('Y-m-d, H:i', $row['time'])?></td>
-						<td><?= $row['user'] ?></td>
+                        <td><?=date('Y-m-d, H:i', $row['time'])?></td>
+                        <td><?= $row['user'] ?></td>
+                        <td>
+                            <?php switch($row['change']) {
+                                case 'C': ?>Created<?php break;
+	                            case 'U': ?>Updated features<?php break;
+                                case 'D': ?>Deleted item<?php break;
+                                case 'M': ?>Moved to <a href="/item/<?= urlencode($this->e($row['other'])) ?>">
+	                                <?= $this->e($row['other']) ?></a><?php break;
+                                case 'L': ?>Marked as lost<?php break;
+                                default: ?>Unknown (<?= $this->e($row['change']) ?>)<?php break;
+                            } ?>
+                        </td>
 					</tr>
 				<?php endforeach ?>
 				</tbody>
