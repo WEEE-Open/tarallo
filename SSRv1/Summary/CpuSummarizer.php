1092c493315bf0b2617bea72184acde30cb6b069
Quadrollopo <davide97@gmail.com>
2019-06-08 18:14:25 +0200

    CPU summarizer added and fixed tests

--- SSRv1/Summary/CpuSummarizer.php
+++ SSRv1/Summary/CpuSummarizer.php
@@ -4,17 +4,57 @@ namespace WEEEOpen\Tarallo\SSRv1\Summary;
 
 
 use WEEEOpen\Tarallo\Server\ItemFeatures;
+use WEEEOpen\Tarallo\SSRv1\FeaturePrinter;
 
 class CpuSummarizer implements Summarizer {
 
 	public static function summarize(ItemFeatures $item): string {
-		// TODO: Implement summarize() method.
-		// Use these functions to obtain the literal parts of the summary string, so they are translatable:
-		// Cores = FeaturePrinter::printableName('cores-n')
-		// Threads = FeaturePrinter::printableName('threads-n')
-		// Socket (CPU) = FeaturePrinter::printableName('cpu-socket')
-		//
-		// e.g. "2 Cores" = FeaturePrinter::printableValue(...) . ' ' . FeaturePrinter::printableName('cores-n')
-		return '';
+
+		$type = $item->getFeature('type');
+		$isa = $item->getFeature('isa');
+		$numCores = $item->getFeature('core-n');
+		$numThreads = $item->getFeature('thread-n');
+		$frequency = $item->getFeature('frequency-hertz');
+		$brand = $item->getFeature('brand');
+		$model = $item->getFeature('model');
+		$socket = $item->getFeature('cpu-socket');
+
+		$architecture = FeaturePrinter::printableValue($type);
+		$architecture .= $isa ? ' ' . FeaturePrinter::printableValue($isa) : ' (architecture?)';
+
+		$coreStats = '';
+		$coreStats .= $numCores ? ' ' . $numCores . ' ' . FeaturePrinter::printableName('core-n') : '';
+		$coreStats .= $numThreads ? ' ' . $numThreads . ' ' . FeaturePrinter::printableName('thread-n') : '';
+		$coreStats .= $frequency ? ' ' . '@ ' . FeaturePrinter::printableValue($frequency) : '';
+
+		if(!$brand && $model) {
+			// To avoid possible confusion with serial numbers...
+			$commercial = ' ' . FeaturePrinter::printableValue($model);
+		} else {
+			$commercial = $brand ? ' ' . FeaturePrinter::printableValue($brand) : '';
+			$commercial .= $model ? ' ' . FeaturePrinter::printableValue($model) : '';
+		}
+
+		if($socket) {
+			$socket = FeaturePrinter::printableValue($socket);
+			$socketOnly = explode(' ', $socket);
+		} else
+			$socket = '';
+
+		if(empty($coreStats) && empty($commercial) && empty($socket))
+			$pretty = FeaturePrinter::printableValue($type);
+		else {
+			$pretty = $architecture;
+			if($coreStats !== '')
+				$pretty .= ",$coreStats";
+			if($commercial !== '')
+				$pretty .= ",$commercial";
+			$pretty .= !empty($socketOnly) ? ', ' . FeaturePrinter::printableName(
+					'cpu-socket'
+				) . ' ' . $socketOnly[0] : '';
+		}
+
+
+		return $pretty;
 	}
 }
