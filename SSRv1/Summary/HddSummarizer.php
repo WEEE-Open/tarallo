f5b6c8d8065841a1351793d07b1c67282b9d41a4
Quadrollopo <davide97@gmail.com>
2019-07-22 15:31:52 +0200

    First implementation of the hardware

--- SSRv1/Summary/HddSummarizer.php
+++ SSRv1/Summary/HddSummarizer.php
@@ -4,11 +4,33 @@ namespace WEEEOpen\Tarallo\SSRv1\Summary;
 
 
 use WEEEOpen\Tarallo\Server\ItemWithFeatures;
+use WEEEOpen\Tarallo\SSRv1\FeaturePrinter;
 
 class HddSummarizer implements Summarizer {
 
 	public static function summarize(ItemWithFeatures $item): string {
-		// TODO: Implement summarize() method.
-		return '';
+		$type = $item->getFeature('type');
+		$capacity = $item->getFeature('capacity-decibyte');
+		$nPorts = $item->getFeature('sata-ports-n');
+		$formFactor = $item->getFeature('hdd-form-factor');
+		$spinRate = $item->getFeature('spin-rate-rpm');
+		$brand = $item->getFeature('brand');
+		$family = $item->getFeature('family');
+		$model = $item->getFeature('model');
+		$os = $item->getFeature('software');
+
+		$hardware = FeaturePrinter::printableValue($type);
+		$hardware .= ' ' . FeaturePrinter::printableValue($capacity);
+		if($nPorts !== null) {
+			if($nPorts->value == 1) {
+				$hardware .= ' ' . FeaturePrinter::printableName('sata-ports-n');
+			} else {
+				$hardware .= ' ' . $nPorts . '×' . FeaturePrinter::printableName('sata-ports-n');
+			}
+		}
+
+		$hardware .= ' ' . FeaturePrinter::printableValue($formFactor);
+		$hardware .= ' ' . FeaturePrinter::printableValue($spinRate);
+		return $hardware;
 	}
 }
