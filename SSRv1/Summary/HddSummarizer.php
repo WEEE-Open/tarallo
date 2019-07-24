12d9c8e3bcb7c7093400dbf17ca6bbf5af96a490
Quadrollopo <davide97@gmail.com>
2019-07-24 19:00:48 +0200

    HddSummarizer booted and ready!

--- SSRv1/Summary/HddSummarizer.php
+++ SSRv1/Summary/HddSummarizer.php
@@ -11,26 +11,105 @@ class HddSummarizer implements Summarizer {
 	public static function summarize(ItemWithFeatures $item): string {
 		$type = $item->getFeature('type');
 		$capacity = $item->getFeature('capacity-decibyte');
-		$nPorts = $item->getFeature('sata-ports-n');
+		$sataPorts = $item->getFeature('sata-ports-n');
+		$idePorts = $item->getFeature('ide-ports-n');
+		$miniIdePorts = $item->getFeature('mini-ide-ports-n');
+		$scsiPorts = $item->getFeature('scsi-sca2-ports-n');
+		$scsi2Ports = $item->getFeature('scsi-db68-ports-n');
 		$formFactor = $item->getFeature('hdd-form-factor');
 		$spinRate = $item->getFeature('spin-rate-rpm');
+		$dataErased = $item->getFeature('data-erased');
+		$smartData = $item->getFeature('smart-data');
+		$surfaceScan = $item->getFeature('surface-scan');
 		$brand = $item->getFeature('brand');
 		$family = $item->getFeature('family');
 		$model = $item->getFeature('model');
 		$os = $item->getFeature('software');
 
+
 		$hardware = FeaturePrinter::printableValue($type);
-		$hardware .= ' ' . FeaturePrinter::printableValue($capacity);
-		if($nPorts !== null) {
-			if($nPorts->value == 1) {
-				$hardware .= ' ' . FeaturePrinter::printableName('sata-ports-n');
+		$hardware .= $capacity ? ' ' . FeaturePrinter::printableValue($capacity) : '';
+		$ports = '';
+		if($sataPorts !== null) {
+			if($sataPorts->value == 1) {
+				$ports .= ' ' . FeaturePrinter::printableName('sata-ports-n');
 			} else {
-				$hardware .= ' ' . $nPorts . '×' . FeaturePrinter::printableName('sata-ports-n');
+				$ports .= ' ' . $sataPorts . '×' . FeaturePrinter::printableName('sata-ports-n');
 			}
 		}
 
-		$hardware .= ' ' . FeaturePrinter::printableValue($formFactor);
-		$hardware .= ' ' . FeaturePrinter::printableValue($spinRate);
-		return $hardware;
+		if($idePorts !== null) {
+			$ports .= $ports ? ' +' : '';
+			if($idePorts->value == 1) {
+				$ports .= ' ' . FeaturePrinter::printableName('ide-ports-n');
+			} else {
+				$ports .= ' ' . $idePorts . '×' . FeaturePrinter::printableName('ide-ports-n');
+			}
+		}
+
+		if($miniIdePorts !== null) {
+			$ports .= $ports ? ' +' : '';
+			if($miniIdePorts->value == 1) {
+				$ports .= ' ' . FeaturePrinter::printableName('mini-ide-ports-n');
+			} else {
+				$ports .= ' ' . $miniIdePorts . '×' . FeaturePrinter::printableName('mini-ide-ports-n');
+			}
+		}
+
+		if($scsiPorts !== null) {
+			$ports .= $ports ? ' +' : '';
+			if($scsiPorts->value == 1) {
+				$ports .= ' ' . FeaturePrinter::printableName('scsi-sca2-ports-n');
+			} else {
+				$ports .= ' ' . $scsiPorts . '×' . FeaturePrinter::printableName('scsi-sca2-ports-n');
+			}
+		}
+
+		if($scsi2Ports !== null) {
+			$ports .= $ports ? ' +' : '';
+			if($scsi2Ports->value == 1) {
+				$ports .= ' ' . FeaturePrinter::printableName('scsi-db68-ports-n');
+			} else {
+				$ports .= ' ' . $scsi2Ports . '×' . FeaturePrinter::printableName('scsi-db68-ports-n');
+			}
+		}
+
+		$hardware .= $ports;
+
+		$hardware .= $formFactor ? ' ' . FeaturePrinter::printableValue($formFactor) : '';
+		$hardware .= $spinRate ? ' ' . FeaturePrinter::printableValue($spinRate) : '';
+
+		$dataErased = $dataErased ? $dataErased->value : '_';
+		$procedures = $dataErased === 'yes' ? 'E' : '_';
+
+		$smartData = $smartData ? $smartData->value : '_';
+		if($smartData === 'ok') {
+			$procedures .= 'S';
+		} else if($smartData === 'old') {
+			$procedures .= 'O';
+		} else if($smartData === 'fail') {
+			$procedures .= 'X';
+		} else {
+			$procedures .= '_';
+		}
+
+		$surfaceScan = $surfaceScan ? $surfaceScan->value : '_';
+		if($surfaceScan === 'pass') {
+			$procedures .= 'P';
+		} else if($surfaceScan === 'fail') {
+			$procedures .= 'X';
+		} else {
+			$procedures .= '_';
+		}
+
+		$commercial = $brand ? FeaturePrinter::printableValue($brand) : '';
+		$commercial .= $family ? ' ' . FeaturePrinter::printableValue($family) : '';
+		$commercial .= $model ? ' ' . FeaturePrinter::printableValue($model) : '';
+
+		$pretty = $hardware . ', ' . $procedures;
+		$pretty .= $commercial ? ', ' . $commercial : '';
+		$pretty .= $os ? ', ' . $os : '';
+
+		return $pretty;
 	}
 }
