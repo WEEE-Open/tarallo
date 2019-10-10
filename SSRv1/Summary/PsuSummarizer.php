8c039178ffc14929583a9e047ac8504f0eacc50b
Quadrollopo <642753Casa>
2019-10-10 17:47:46 +0200

    Ultrasummarizer ready!

--- SSRv1/Summary/PsuSummarizer.php
+++ SSRv1/Summary/PsuSummarizer.php
@@ -9,12 +9,24 @@ use WEEEOpen\Tarallo\SSRv1\FeaturePrinter;
 class PsuSummarizer implements Summarizer {
 
 	public static function summarize(ItemWithFeatures $item): string {
-		$type = FeaturePrinter::printableValue($item->getFeature('type'));
+		$type = $item->getFeature('type');
+		$formFactor = $item->getFeature('psu-form-factor');
+		$powerWatt = $item->getFeature('power-rated-watt');
+		$color = $item->getFeature('color');
+
+		$type = FeaturePrinter::printableValue($type);
+		$type .= $formFactor ? ' ' . FeaturePrinter::printableValue($formFactor) : '';
+		$type .= $powerWatt ? ' ' . FeaturePrinter::printableValue($powerWatt) : '';
+		$color = $color ? ', ' . FeaturePrinter::printableValue($color) : '';
+
+
 		$power = PartialSummaries::summarizePowerconnectors($item);
+		$power = $power ? " ($power)" : '';
 		$commercial = PartialSummaries::summarizeCommercial($item);
+		$commercial = $commercial ? ", $commercial" : '';
 
 		// TODO: finish this
-		$pretty = "$type ($power), $commercial";
+		$pretty = "$type" . "$power" . "$color" . "$commercial";
 
 		return $pretty;
 	}
