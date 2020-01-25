2bfbe07e194ad044929bbf8809d6d81e912ebc8b
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2020-01-25 19:47:03 +0100

    No link for products with 0 items
    Just for increased readability, the page that lists the 0 items still exists

--- src/SSRv1/templates/products.php
+++ src/SSRv1/templates/products.php
@@ -13,19 +13,25 @@ $this->layout(
 ?>
 <h2>All products</h2>
 
-<table>
-	<thead>
-	<tr>
-		<td>Product</td>
-		<td>Items</td>
-	</tr>
-	</thead>
-	<tbody>
-	<?php foreach($products as $row): /** @var \WEEEOpen\Tarallo\ProductCode $product */ $product = $row[0]; $count = $row[1] ?>
+<div class="tablewrapper">
+	<table>
+		<thead>
 		<tr>
-			<td><a href="/product/<?=rawurlencode($product->getBrand())?>/<?=rawurlencode($product->getModel())?>/<?=rawurlencode($product->getVariant())?>"><?= $this->e($product->getBrand()) . ' ' . $this->e($product->getModel()) . rtrim(' ' . $this->e($product->getVariantOrEmpty())) ?></a></td>
-			<td><a href="/product/<?=rawurlencode($product->getBrand())?>/<?=rawurlencode($product->getModel())?>/<?=rawurlencode($product->getVariant())?>/items"><?= $count ?></a></td>
+			<td>Product</td>
+			<td>Items</td>
 		</tr>
-	<?php endforeach ?>
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+		<?php foreach($products as $row): /** @var \WEEEOpen\Tarallo\ProductCode $product */ $product = $row[0]; $count = $row[1] ?>
+			<tr>
+				<td><a href="/product/<?=rawurlencode($product->getBrand())?>/<?=rawurlencode($product->getModel())?>/<?=rawurlencode($product->getVariant())?>"><?= $this->e($product->getBrand()) . ' ' . $this->e($product->getModel())?><small><?= rtrim(' ' . $this->e($product->getVariantOrEmpty())) ?></small></a></td>
+				<?php if($count === 0): ?>
+				<td>0</td>
+				<?php else: ?>
+				<td><a href="/product/<?=rawurlencode($product->getBrand())?>/<?=rawurlencode($product->getModel())?>/<?=rawurlencode($product->getVariant())?>/items"><?= $count ?></a></td>
+				<?php endif ?>
+			</tr>
+		<?php endforeach ?>
+		</tbody>
+	</table>
+</div>
