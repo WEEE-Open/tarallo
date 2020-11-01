721ce0e1bf5d8abe3bec1b75ca41fbd310eb854a
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2020-11-01 23:59:57 +0100

    Fix #150

--- bin/update.php
+++ bin/update.php
@@ -15,9 +15,9 @@ require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . 
 
 if(isset($argv[1]) && $argv[1] === 'test_db') {
 	// Hardcoding. For the test database that exists only in development. This won't harm anything in production,
-	// unless you have a root/root account and a tarallo_test database with production data...
-	define('TARALLO_DB_USERNAME', 'root');
-	define('TARALLO_DB_PASSWORD', 'root');
+	// unless you have a tarallo_test database with production data...
+	define('TARALLO_DB_USERNAME', 'tarallo');
+	define('TARALLO_DB_PASSWORD', 'thisisnottheproductionpassword');
 	define('TARALLO_DB_DSN', 'mysql:dbname=tarallo_test;host=localhost;charset=utf8mb4');
 } else {
 	require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
