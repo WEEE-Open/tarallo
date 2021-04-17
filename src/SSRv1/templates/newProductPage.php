2196cd7b3a27e79614bc2910d02bc929703107ea
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-04-17 17:51:48 +0200

    Fix undefined variable $importedFrom when splitting a product from an item

--- src/SSRv1/templates/newProductPage.php
+++ src/SSRv1/templates/newProductPage.php
@@ -1,9 +1,9 @@
 <?php
 /** @var \WEEEOpen\Tarallo\User $user */
 /** @var \WEEEOpen\Tarallo\ProductIncomplete|\WEEEOpen\Tarallo\Product|null $base */
-
 $base = $base ?? null;
 /** @var int $importedFrom|null */
+$importedFrom = $importedFrom ?? null;
 
 $this->layout('main', ['title' => 'New product', 'currentPage' => 'product new']);
 $this->insert('newProduct', [
