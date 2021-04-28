cc5144915191593ee8cdf14f9dfb74abe79ca387
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-04-29 01:35:04 +0200

    Add Item(DAO) tests
    Coverage is increasing

--- tests/Database/ItemDAOTest.php
+++ tests/Database/ItemDAOTest.php
@@ -6,7 +6,10 @@ use WEEEOpen\Tarallo\Database\DuplicateItemCodeException;
 use WEEEOpen\Tarallo\Feature;
 use WEEEOpen\Tarallo\Item;
 use WEEEOpen\Tarallo\ItemCode;
+use WEEEOpen\Tarallo\ItemTraitCode;
+use WEEEOpen\Tarallo\ItemTraitOptionalCode;
 use WEEEOpen\Tarallo\NotFoundException;
+use WEEEOpen\Tarallo\Product;
 use WEEEOpen\Tarallo\ValidationException;
 
 class ItemDAOTest extends DatabaseTest {
@@ -47,8 +50,10 @@ class ItemDAOTest extends DatabaseTest {
 		$this->assertContainsOnly(Item::class, $newCase->getContent(), null, 'Only Items are contained in an Item');
 		foreach($newCase->getContent() as $child) {
 			/** @var Item $child */
-			$this->assertTrue($child->getCode() === 'SATAna1' || $child->getCode() === 'SATAna2',
-				'Sub-Item is one of the two expected items, ' . (string) $child);
+			$this->assertTrue(
+				$child->getCode() === 'SATAna1' || $child->getCode() === 'SATAna2',
+				'Sub-Item is one of the two expected items, ' . (string) $child
+			);
 			// this works because the two items are identical except for the code...
 			$newFeatures = $child->getFeatures();
 			$oldFeatures = $case->getContent()[0]->getFeatures();
@@ -56,8 +61,10 @@ class ItemDAOTest extends DatabaseTest {
 			foreach($oldFeatures as $name => $feature) {
 				$value = $feature->value;
 				$this->assertTrue(isset($newFeatures[$name]), "Feature $name should still exist");
-				$this->assertEquals($value, $newFeatures[$name]->value,
-					"Sub-Item $child should have $name=$value as before");
+				$this->assertEquals(
+					$value, $newFeatures[$name]->value,
+					"Sub-Item $child should have $name=$value as before"
+				);
 			}
 			$this->assertTrue(empty($child->getContent()), "No children of child Item $child should exist");
 		}
@@ -84,8 +91,10 @@ class ItemDAOTest extends DatabaseTest {
 		$this->assertTrue($db->itemDAO()->itemExists($deleteMe), 'Item should still exist');
 		$this->assertFalse($db->itemDAO()->itemVisible($deleteMe), 'Item shouldn\'t be visible');
 		$afterTime = new \DateTime($db->itemDAO()->itemDeletedAt($deleteMe), new \DateTimeZone('UTC'));
-		$this->assertGreaterThanOrEqual(0, $afterTime->getTimestamp() - $beforeTime->getTimestamp(),
-			'Item should have a valid deletion date');
+		$this->assertGreaterThanOrEqual(
+			0, $afterTime->getTimestamp() - $beforeTime->getTimestamp(),
+			'Item should have a valid deletion date'
+		);
 		$this->assertInstanceOf(Item::class, $db->itemDAO()->getItem($deleteMe));
 	}
 
@@ -145,8 +154,10 @@ class ItemDAOTest extends DatabaseTest {
 		$recovered = $db->itemDAO()->getItem($saveMe);
 		$this->assertInstanceOf(Item::class, $recovered, 'Can grab item from database');
 		$this->assertEmpty($recovered->getPath(), 'Item isn\'t placed anywhere');
-		$this->assertEquals(1, count($recovered->getFeatures()),
-			'Item contains the same number of features it contained before');
+		$this->assertEquals(
+			1, count($recovered->getFeatures()),
+			'Item contains the same number of features it contained before'
+		);
 
 		// Prevent integrity constraint violations due to duplicate audit entries...
 		$this->getPdo()
@@ -326,4 +337,235 @@ class ItemDAOTest extends DatabaseTest {
 		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
 	}
 
+	/**
+	 * @covers \WEEEOpen\Tarallo\Database\ItemDAO
+	 * @covers \WEEEOpen\Tarallo\Item
+	 * @covers \WEEEOpen\Tarallo\Product
+	 * @noinspection DuplicatedCode
+	 */
+	public function testItemWithProductSerializable() {
+		$db = $this->getDb();
+
+		$where = (new Item('Chernobyl'));
+
+		$product = (new Product('AsStone', 'AS-125B-X', 'default'))
+			->addFeature(new Feature('brand', 'AsStone'))
+			->addFeature(new Feature('model', 'AS-125B-X'))
+			->addFeature(new Feature('variant', 'default'))
+			->addFeature(new Feature('type', 'motherboard'))
+			->addFeature(new Feature('color', 'brown'));
+		$db->productDAO()->addProduct($product);
+
+		$mobo = (new Item('B123'))
+			->addFeature(new Feature('brand', 'AsStone'))
+			->addFeature(new Feature('model', 'AS-125B-X'))
+			->addFeature(new Feature('variant', 'default'))
+			->addFeature(new Feature('working', 'yes'));
+		$cpu = (new Item('C123'))
+			->addFeature(new Feature('brand', "Intel"))
+			->addFeature(new Feature('frequency-hertz', (int) (2.67 * 1000 * 1000 * 1000)))
+			->addFeature(new Feature('type', 'cpu'));
+		$mobo->addContent($cpu);
+		$where->addContent($mobo);
+
+		$db->itemDAO()->addItem($where);
+
+		$getMe = new ItemCode('Chernobyl');
+		$result = $db->itemDAO()->getItem($getMe);
+
+		$json = json_encode($result);
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+
+		$decoded = json_decode($json, true);
+		$this->assertArrayHasKey("contents", $decoded);
+		$this->assertCount(1, $decoded["contents"]);
+		$this->assertArrayHasKey("code", $decoded["contents"][0]);
+		$this->assertEquals("B123", $decoded["contents"][0]["code"]);
+		$this->assertArrayNotHasKey("product", $decoded["contents"][0]);
+		$this->assertArrayHasKey("features", $decoded["contents"][0]);
+		$this->assertArrayHasKey("color", $decoded["contents"][0]["features"]);
+		$this->assertEquals("brown", $decoded["contents"][0]["features"]["color"]);
+
+		$result->setSeparate();
+		$json = json_encode($result);
+
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+		$decoded = json_decode($json, true);
+		$this->assertArrayHasKey("contents", $decoded);
+		$this->assertCount(1, $decoded["contents"]);
+		$this->assertArrayHasKey("code", $decoded["contents"][0]);
+		$this->assertEquals("B123", $decoded["contents"][0]["code"]);
+		$this->assertArrayHasKey("product", $decoded["contents"][0]);
+		$this->assertArrayHasKey("features", $decoded["contents"][0]["product"]);
+		$this->assertArrayHasKey("color", $decoded["contents"][0]["product"]["features"]);
+		$this->assertEquals("brown", $decoded["contents"][0]["product"]["features"]["color"]);
+	}
+
+	/**
+	 * @covers \WEEEOpen\Tarallo\Database\ItemDAO
+	 * @covers \WEEEOpen\Tarallo\Item
+	 */
+	public function testLostItem() {
+		$db = $this->getDb();
+
+		$where = (new Item('Chernobyl'));
+
+		$mobo = (new Item('B123'))
+			->addFeature(new Feature('brand', 'AsStone'))
+			->addFeature(new Feature('model', 'AS-125B-X'))
+			->addFeature(new Feature('variant', 'default'))
+			->addFeature(new Feature('type', 'motherboard'))
+			->addFeature(new Feature('working', 'yes'));
+		$cpu = (new Item('C123'))
+			->addFeature(new Feature('brand', "Intel"))
+			->addFeature(new Feature('frequency-hertz', (int) (2.67 * 1000 * 1000 * 1000)))
+			->addFeature(new Feature('type', 'cpu'));
+		$mobo->addContent($cpu);
+		$where->addContent($mobo);
+
+		$db->itemDAO()->addItem($where);
+
+		$getMe = new ItemCode('C123');
+		$result = $db->itemDAO()->getItem($getMe);
+		$this->assertEquals('B123', $result->getParent()->getCode());
+
+		$db->itemDAO()->loseItem($getMe);
+
+		$getMe = new ItemCode('C123');
+		$result = $db->itemDAO()->getItem($getMe);
+		$this->assertNull($result->getParent());
+
+		$this->assertNotNull($result->getLostAt());
+		$this->assertNull($result->getDeletedAt());
+
+		$json = json_encode($result);
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+		$decoded = json_decode($json, true);
+
+		$this->assertArrayHasKey("lost_at", $decoded);
+		$this->assertArrayNotHasKey("deleted_at", $decoded);
+		$this->assertIsString($decoded["lost_at"]);
+		$this->assertArrayNotHasKey("parent", $decoded);
+		$this->assertArrayNotHasKey("contents", $decoded);
+	}
+
+	/**
+	 * @covers \WEEEOpen\Tarallo\Database\ItemDAO
+	 * @covers \WEEEOpen\Tarallo\Item
+	 */
+	public function testDeletedItem() {
+		$db = $this->getDb();
+
+		$where = (new Item('Chernobyl'));
+
+		$mobo = (new Item('B321'))
+			->addFeature(new Feature('brand', 'AsStone'))
+			->addFeature(new Feature('model', 'AS-125B-X'))
+			->addFeature(new Feature('variant', 'default'))
+			->addFeature(new Feature('type', 'motherboard'))
+			->addFeature(new Feature('working', 'yes'));
+		$cpu = (new Item('C321'))
+			->addFeature(new Feature('brand', "Intel"))
+			->addFeature(new Feature('frequency-hertz', (int) (2.50 * 1000 * 1000 * 1000)))
+			->addFeature(new Feature('type', 'cpu'));
+		$mobo->addContent($cpu);
+		$where->addContent($mobo);
+		$db->itemDAO()->addItem($where);
+
+		$getMe = new ItemCode('C321');
+		$result = $db->itemDAO()->getItem($getMe);
+		$this->assertEquals('B321', $result->getParent()->getCode());
+
+		$db->itemDAO()->deleteItem($getMe);
+
+		$getMe = new ItemCode('C321');
+		$result = $db->itemDAO()->getItem($getMe);
+		$this->assertNull($result->getParent());
+
+		$this->assertNull($result->getLostAt());
+		$this->assertNotNull($result->getDeletedAt());
+
+		$json = json_encode($result);
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+		$decoded = json_decode($json, true);
+
+		$this->assertArrayHasKey("deleted_at", $decoded);
+		$this->assertArrayNotHasKey("lost_at", $decoded);
+		$this->assertIsString($decoded["deleted_at"]);
+		$this->assertArrayNotHasKey("parent", $decoded);
+		$this->assertArrayNotHasKey("contents", $decoded);
+	}
+
+	/**
+	* @covers \WEEEOpen\Tarallo\Database\ItemDAO
+	*/
+	public function testCannotLoseIntermediateItem() {
+		$db = $this->getDb();
+
+		$where = (new Item('Chernobyl'));
+
+		$mobo = (new Item('B123'))
+			->addFeature(new Feature('brand', 'AsStone'))
+			->addFeature(new Feature('model', 'AS-125C-X'))
+			->addFeature(new Feature('variant', 'default'))
+			->addFeature(new Feature('type', 'motherboard'))
+			->addFeature(new Feature('working', 'yes'));
+		$cpu = (new Item('C123'))
+			->addFeature(new Feature('brand', "Intel"))
+			->addFeature(new Feature('frequency-hertz', (int) (2.67 * 1000 * 1000 * 1000)))
+			->addFeature(new Feature('type', 'cpu'));
+		$mobo->addContent($cpu);
+		$where->addContent($mobo);
+		$db->itemDAO()->addItem($where);
+
+		$this->expectException(ValidationException::class);
+		$db->itemDAO()->loseItem(new ItemCode('B123'));
+	}
+
+	/**
+	 * @covers \WEEEOpen\Tarallo\Database\ItemDAO
+	 */
+	public function testCannotDeleteIntermediateItem() {
+		$db = $this->getDb();
+
+		$where = (new Item('Chernobyl'));
+
+		$mobo = (new Item('B123'))
+			->addFeature(new Feature('brand', 'AsStone'))
+			->addFeature(new Feature('model', 'AS-126C-X'))
+			->addFeature(new Feature('variant', 'default'))
+			->addFeature(new Feature('type', 'motherboard'))
+			->addFeature(new Feature('working', 'yes'));
+		$cpu = (new Item('C222'))
+			->addFeature(new Feature('brand', "Intel"))
+			->addFeature(new Feature('frequency-hertz', (int) (2.67 * 1000 * 1000 * 1000)))
+			->addFeature(new Feature('type', 'cpu'));
+		$mobo->addContent($cpu);
+		$where->addContent($mobo);
+
+		$db->itemDAO()->addItem($where);
+
+		$this->expectException(ValidationException::class);
+		$db->itemDAO()->deleteItem(new ItemCode('B123'));
+	}
+
+	/**
+	 * @covers \WEEEOpen\Tarallo\Database\ItemDAO
+	 */
+	public function testCannotLoseNotExistingItem() {
+		$db = $this->getDb();
+
+		$this->expectException(NotFoundException::class);
+		$db->itemDAO()->deleteItem(new ItemCode('DOESNOTEXIST'));
+	}
+
+	/**
+	 * @covers \WEEEOpen\Tarallo\Database\ItemDAO
+	 */
+	public function testCannotDeleteNotExistingItem() {
+		$db = $this->getDb();
+
+		$this->expectException(NotFoundException::class);
+		$db->itemDAO()->deleteItem(new ItemCode('DOESNOTEXIST'));
+	}
 }
