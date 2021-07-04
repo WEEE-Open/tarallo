56948d5b421868d71764d277fca952c5fa3964ba
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-07-04 21:56:28 +0200

    Darker grey

--- src/SSRv1/TemplateUtilities.php
+++ src/SSRv1/TemplateUtilities.php
@@ -94,8 +94,8 @@ class TemplateUtilities implements ExtensionInterface {
 	}
 
 	public function colorToHtml(string $color): string {
-		$from = ['sip-brown', 'brown', 'orange', 'golden', 'yellowed', '-'];
-		$to   = ['#CB8', 'saddlebrown', 'darkorange', 'gold', 'lightyellow', ''];
+		$from = ['sip-brown', 'brown', 'darkgrey', 'orange', 'golden', 'yellowed', '-'];
+		$to   = ['#CB8', 'saddlebrown', 'dimgrey', 'darkorange', 'gold', 'lightyellow', ''];
 		return str_replace($from, $to, $color);
 	}
 
