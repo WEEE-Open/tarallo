6056d17f33984d42cd4063cf1513ae8de4150ecf
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-07-04 22:19:32 +0200

    Sort things

--- src/SSRv1/templates/info/todo.php
+++ src/SSRv1/templates/info/todo.php
@@ -11,14 +11,20 @@ $pclass = '';
 if(isset($included) && $included) {
 	$pclass = ' class="my-0"';
 }
+
+$todosWithHumanReadableFeatures = [];
+foreach($todos as $feature => $items) {
+	$todosWithHumanReadableFeatures[WEEEOpen\Tarallo\SSRv1\FeaturePrinter::printableValue(new \WEEEOpen\Tarallo\Feature('todo', $feature))] =
+		$items;
+}
+ksort($todosWithHumanReadableFeatures);
 ?>
 <h2>What's to do?</h2>
 <div class="row">
-<?php foreach($todos as $feature => $items): ?>
+<?php foreach($todosWithHumanReadableFeatures as $feature => $items): ?>
     <?php if(count($items) > 0): ?>
-	<div class="stats list col-12" style="box-shadow: 0 0.2rem 0.4rem #b9b9b9">
-        <p<?=$pclass?>><?= WEEEOpen\Tarallo\SSRv1\FeaturePrinter::printableValue(new \WEEEOpen\Tarallo\Feature('todo',
-				$feature)) ?> <small>(<?= count($items) ?> items, max 100 shown)</small></p>
+	<div class="stats list col-12 py-2" style="box-shadow: 0 0.2rem 0.4rem #b9b9b9">
+        <p<?=$pclass?>><?= $feature ?> <small>(<?= count($items) ?> items, max 100 shown)</small></p>
         <div>
             <?php foreach($items as $item): ?>
                 <a href="/item/<?= $this->e(rawurlencode($item)) ?>"><?= $this->e($item) ?></a>
@@ -30,7 +36,7 @@ if(isset($included) && $included) {
 </div>
 <div class="row">
 <?php if(!empty($missingSmartOrSurfaceScan)): ?>
-	<div class="stats list col-12" style="box-shadow: 0 0.2rem 0.4rem #b9b9b9">
+	<div class="stats list col-12 py-2" style="box-shadow: 0 0.2rem 0.4rem #b9b9b9">
 		<p<?=$pclass?>>Working HDDs with missing SMART or Surface Scan: use the turbofresa on them! <small>(<?=count($missingSmartOrSurfaceScan)?> HDDs, max 200 shown)</small></p>
 		<div>
 			<?php foreach($missingSmartOrSurfaceScan as $item): ?>
