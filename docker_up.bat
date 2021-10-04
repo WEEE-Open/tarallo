094a197708c4e416f5a20f0389dac6fd67d37431
Quadrollopo <davide97@gmail.com>
2021-10-04 19:42:16 +0200

    Update with wait-for-db

--- docker_up.bat
+++ docker_up.bat
@@ -1,6 +1,4 @@
-start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-timeout /t 20 /nobreak
 docker-compose up -d
-timeout /t 10 /nobreak
+docker-compose exec -T app php /var/www/html/bin/wait-for-db
 docker-compose exec -T app php /var/www/html/bin/update.php
 docker-compose exec -T app php /var/www/html/bin/create_example_data.php
