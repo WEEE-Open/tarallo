c61e414275bd49e874f3dcbcf56a791620688824
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2021-11-13 15:03:07 +0100

    Typo

--- config/config-example.php
+++ config/config-example.php
@@ -2,7 +2,7 @@
 define('TARALLO_DB_USERNAME', 'user');
 define('TARALLO_DB_PASSWORD', 'pass');
 define('TARALLO_DB_DSN', 'mysql:dbname=tarallo;host=127.0.0.1;charset=utf8mb4');
-define('TARALLO_CACHE_ENABLED', false); // Set to true to enable FastRoute cache (use in production only, leave false in developement)
+define('TARALLO_CACHE_ENABLED', false); // Set to true to enable FastRoute cache (use in production only, leave false in development)
 define('TARALLO_DEVELOPMENT_ENVIRONMENT', true); // Set to false or delete in production
 define('TARALLO_POST_GRACE_TIME', 1800);
 define('TARALLO_OIDC_ISSUER', 'https://sso.example.com/auth/realms/master');
