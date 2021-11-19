fa4fcaa494c1a309edaa797f9c230b754bae87a5
Bruno <ichigoplayer18@gmail.com>
2021-11-19 15:50:47 +0100

    fixed a problem in front end

--- src/SSRv1/templates/donations.php
+++ src/SSRv1/templates/donations.php
@@ -1,26 +1,8 @@
 <?php
 /** @var \WEEEOpen\Tarallo\User $user */
-/** @var \WEEEOpen\Tarallo\Donations[] $donations */
+/** @var array $donations */
 $this->layout('main', ['title' => 'Donations', 'user' => $user]);
 ?>
-
-<!doctype html>
-<html lang="<?= /* $lang */ 'en-US' ?>">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="shortcut icon" type="image/png" href="/static/favicon.png"/>
-	<link rel="stylesheet" href="/static/bootstrap.custom.min.css">
-	<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
-	<script src="https://unpkg.com/@popperjs/core@2"></script>
-	<script src="https://unpkg.com/tippy.js@6"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4/dist/js/bootstrap.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js"></script>
-	<script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
-</head>
-<body>
     <div class="container">
 		<div class="row">
         	<h2 class="col-8">Donations</h2>
@@ -30,7 +12,7 @@ $this->layout('main', ['title' => 'Donations', 'user' => $user]);
 		<table class="table table-striped table-borderless">
 			<thead class="thead-dark">
 				<tr>
-					<th class="col-4">Donation Recipient</th>
+					<th class="col-4">Donation Name</th>
 					<th class="col">Donation Date</th>
 					<th class="col">Completed</th>
 					<th class="col"></th>
@@ -58,4 +40,3 @@ $this->layout('main', ['title' => 'Donations', 'user' => $user]);
 				<?php endforeach ?>
 			</tbody>
     </div>
-</body>
