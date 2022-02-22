7fb9cb6b468f96805de8fee280736782f77187d0
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2022-02-22 22:25:03 +0100

    Manual lint fix

--- src/SSRv1/Controller.php
+++ src/SSRv1/Controller.php
@@ -997,16 +997,16 @@ class Controller implements RequestHandlerInterface
 		$importsAggregated = [];
 		foreach ($imports as $importElement) {
 			try {
-				if(isset($importElement['JSON'])) {
+				if (isset($importElement['JSON'])) {
 					$json = json_decode($importElement['JSON'], true);
 				} else {
 					$json = [];
 				}
-				if($importElement['Type'] === 'I') {
+				if ($importElement['Type'] === 'I') {
 					$parsed = ItemBuilder::ofArrayFeatures($json);
 					$importElement['Exists'] = false;
-				} else if($importElement['Type'] === 'P') {
-					if(isset($json['brand']) && is_string($json['brand']) && strlen($json['brand']) > 0 && isset($json['model']) && is_string($json['model']) && strlen($json['model']) > 0) {
+				} elseif ($importElement['Type'] === 'P') {
+					if (isset($json['brand']) && is_string($json['brand']) && strlen($json['brand']) > 0 && isset($json['model']) && is_string($json['model']) && strlen($json['model']) > 0) {
 						$parsed = ProductBuilder::ofArray($json, $json['brand'], $json['model'], $json['variant'] ?? ProductCode::DEFAULT_VARIANT);
 						$importElement['Exists'] = $db->productDAO()->productExists($parsed);
 						$importElement['EncodedUrl'] = '/product/' . rawurlencode($parsed->getBrand()) . '/' . rawurlencode($parsed->getModel()) . '/' . rawurlencode($parsed->getVariant());
@@ -1019,7 +1019,7 @@ class Controller implements RequestHandlerInterface
 					$importElement['Exists'] = false;
 				}
 				$importElement['SuperSummary'] = Summary::peelBulkItem($parsed);
-			} catch(FeatureValidationException $e) {
+			} catch (FeatureValidationException $e) {
 				$importElement['Exists'] = $importElement['Exists'] ?? false;
 				$importElement['SuperSummary'] = "Parse error: " . $e->getMessage();
 			}
