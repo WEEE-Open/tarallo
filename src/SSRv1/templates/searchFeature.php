5fdd443bd8d68cfcbefc47a10863ba28378f9bc2
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2022-05-15 14:22:40 +0200

    Improve feature search page

--- src/SSRv1/templates/searchFeature.php
+++ src/SSRv1/templates/searchFeature.php
@@ -8,7 +8,7 @@
 $ultra = $this->getUltraFeatures([$feature])[0];
 $this->layout('main', ['title' => 'Search items having ' . $this->e($ultra->pname) . ' = ' . $this->e($ultra->pvalue), 'user' => $user, 'currentPage' => 'search feature', 'container' => true]);
 ?>
-<h5>Items with "<?= $this->e($ultra->pname) ?>" equal to <?= $this->e($ultra->pvalue) ?> <small>(<?= count($items) ?>, max <?= (int) $limit ?>)</small></h5>
+<h5>Items with "<?= $this->e($ultra->pname) ?>" equal to <?= $this->e($ultra->pvalue) ?><?= count($items) === $limit ? ' <small>(max ' . $limit . ')</small>' : '' ?></h5>
 <?php if (count($items) > 0) : ?>
 	<ul class="list-unstyled underlinelinks">
 	<?php foreach ($items as $item) :
