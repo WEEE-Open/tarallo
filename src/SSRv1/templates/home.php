6d8bf4c48439fc050f6d17fc43501b30767097ba
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2022-05-16 20:32:40 +0200

    Move TODOs up and key combinations down

--- src/SSRv1/templates/home.php
+++ src/SSRv1/templates/home.php
@@ -7,7 +7,7 @@
 $this->layout('main', ['title' => 'Home', 'user' => $user, 'container' => true]);
 //date_default_timezone_set('Europe/Rome');
 ?>
-
+<?php $this->insert('info::todo', ['todos' => $todos, 'checks' => $checks, 'missingSmartOrSurfaceScan' => $missingSmartOrSurfaceScan, 'toTest' => $toTest, 'included' => true]) ?>
 <h2>Useful key combinations</h2>
 <p>In editor mode, select a feature and...</p>
 <ul class="list-unstyled">
@@ -16,4 +16,3 @@ $this->layout('main', ['title' => 'Home', 'user' => $user, 'container' => true])
 	<li><kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>L</kbd> convert to lowercase</li>
 	<li><kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>Y</kbd> Convert To Title Case</li>
 </ul>
-<?php $this->insert('info::todo', ['todos' => $todos, 'checks' => $checks, 'missingSmartOrSurfaceScan' => $missingSmartOrSurfaceScan, 'toTest' => $toTest, 'included' => true]) ?>
