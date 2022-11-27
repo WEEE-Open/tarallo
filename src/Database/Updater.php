5d827b640e1c8d12d0320fb432835f15f4a854fa
Enrico <39310565+Leone25@users.noreply.github.com>
2022-11-27 14:51:25 +0100

    Added update procedures for db

--- src/Database/Updater.php
+++ src/Database/Updater.php
@@ -692,6 +692,30 @@ END;"
   COLLATE = utf8mb4_unicode_ci;'
 					);
 					break;
+					case 21:
+						$this->exec("DROP TRIGGER IF EXISTS CascadeItemCodeUpdateForReal");
+						$this->exec("
+CREATE TRIGGER CascadeItemCodeUpdateForReal
+BEFORE UPDATE
+ON Item
+FOR EACH ROW
+BEGIN
+	IF(NEW.Code <> OLD.Code) THEN
+		SET FOREIGN_KEY_CHECKS = 0;
+		UPDATE ItemFeature
+		SET Code=NEW.Code
+		WHERE Code=OLD.Code;
+		UPDATE Tree
+		SET Ancestor=NEW.Code
+		WHERE Ancestor=OLD.Code;
+		UPDATE Tree
+		SET Descendant=NEW.Code
+		WHERE Descendant=OLD.Code;
+		SET FOREIGN_KEY_CHECKS = 1;
+	END IF;
+END;"
+						);
+						break;
 				default:
 					throw new \RuntimeException('Schema version larger than maximum');
 			}
